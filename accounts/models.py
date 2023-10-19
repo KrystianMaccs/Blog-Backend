d20b61ef65f83893697afb3303c589b6b0950460
Krystian Maccs <krystianmaccs@gmail.com>
2023-10-19 02:17:30 +0100

    chore: configured celery worker

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,4 @@
 import uuid
-import requests
-from .api import fetch_user_geo_data, extract_user_geo_data
 
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
@@ -41,53 +39,22 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def get_short_name(self):
         return self.username
-    
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        if not self.id:
-            api_response = fetch_user_geo_data(self.id)
-            user_geo_data = extract_user_geo_data(api_response)
-            try:
-                user_geo = UserGeoData(user=self, **user_geo_data)
-                user_geo.save()
-            except Exception as e:
-                print(f"Error creating UserGeoData: {e}")
-    
-    
+
+class Security(models.Model):
+    is_vpn = models.BooleanField(default=False)
     
 class UserGeoData(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=False)
     ip_address = models.GenericIPAddressField(default="0.0.0.0")
     city = models.CharField(max_length=50)
     region_iso_code = models.CharField(max_length=5)
     country_code = models.CharField(max_length=5)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
-    security = models.BooleanField(default=False)
-    
-    # def save(self, *args, **kwargs):
-    #     super().save(*args, **kwargs)
-    #     if not self.usergeo:
-    #         api_response = fetch_user_geo_data(self.id)
-    #         user_geo_data = extract_user_geo_data(api_response)
-    #         try:
-    #             user_geo = UserGeoData(user=self, **user_geo_data)
-    #             user_geo.save()
-    #         except Exception as e:
-    #             print(f"Error creating UserGeoData: {e}")
+    security = models.OneToOneField(Security, on_delete=models.CASCADE, null=False)   
 
-
-
-    # def save(self, *args, **kwargs):
-    #     super().save(*args, **kwargs)
-    #     api_response = fetch_user_geo_data(self.id)
-    #     user_geo_data = extract_user_geo_data(api_response)
-    #     user_geo = UserGeoData(user=self, **user_geo_data)
-    #     user_geo.save()
-        
-
-# class Holiday(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     holiday_name = models.CharField(max_length=120)
-#     holiday_type = models.CharField(max_length=120)
-#     date = models.DateTimeField(auto_now=True)
+class Holiday(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=False)
+    holiday_name = models.CharField(max_length=120)
+    holiday_type = models.CharField(max_length=120)
+    date = models.DateTimeField(auto_now=True)
--- accounts/tasks.py
+++ accounts/tasks.py
@@ -1,42 +1,73 @@
-# from celery import shared_task
-# from .models import User, UserGeoData
-# import requests
+import requests
+from celery import shared_task
+from .models import UserGeoData, Security, Holiday
+import datetime
 
-# @shared_task
-# def populate_user_geo_data(id):
-#     user = User.objects.get(id=id)
-#     geolocation_data = requests.get('https://ipgeolocation.abstractapi.com/v1/?api_key=d77f75a5593e4073a6eb1bef9c35e929')
-    
-#     ip_address = geolocation_data['ip_address']
-#     city = geolocation_data['city']
-#     region_iso_code = geolocation_data['region_iso_code']
-#     country_code = geolocation_data['country_code']
-#     longitude = geolocation_data['longitude']
-#     latitude = geolocation_data['latitude']
 
-#     # Create UserGeoData object and save it
-#     user_geo_data = UserGeoData(
-#         user=user,
-#         ip_address=ip_address,
-#         city=city,
-#         region_iso_code=region_iso_code,
-#         country_code=country_code,
-#         longitude=longitude,
-#         latitude=latitude
-#     )
-#     user_geo_data.save()
 
-# # @shared_task
-# # def check_for_holiday(id):
-# #     user = User.objects.get(id=id)
-# #     holiday_data = requests.get('("https://holidays.abstractapi.com/v1/?api_key=9cb9967be3054f95a7f4c80fae708370&country={country_code}&year=2020&month=12&day=25")', params={'country_code': user.usergeo.country_code, 'date': user.date_joined})
+GEO_API_URL = 'https://ipgeolocation.abstractapi.com/v1/?api_key=d77f75a5593e4073a6eb1bef9c35e929'
+HOLIDAY_API_BASE_URL = 'https://holidays.abstractapi.com/v1/?api_key=9cb9967be3054f95a7f4c80fae708370'
 
-# #     if holiday_data['is_holiday']:
-# #         holiday_name = holiday_data['holiday_name']
-# #         holiday_type = holiday_data['holiday_type']
-# #         holiday = Holiday(
-# #             user=user,
-# #             holiday_name=holiday_name,
-# #             holiday_type=holiday_type
-# #         )
-# #         holiday.save()
+@shared_task
+def fetch_and_save_geodata():
+    try:
+        response = requests.get(GEO_API_URL)
+        response.raise_for_status()
+
+        data = response.json()
+        
+        ip_address = data['ip_address']
+        city = data['city']
+        region_iso_code = data['region_iso_code']
+        country_code = data['country_code']
+        longitude = data['longitude']
+        latitude = data['latitude']
+        is_vpn = data['security']['is_vpn']
+
+        user_geo_data, created = UserGeoData.objects.update_or_create(
+            ip_address=ip_address,
+            defaults={
+                'city': city,
+                'region_iso_code': region_iso_code,
+                'country_code': country_code,
+                'longitude': longitude,
+                'latitude': latitude,
+            }
+        )
+        security, created = Security.objects.update_or_create(
+            defaults={'is_vpn': is_vpn}
+        )
+        user_geo_data.security = security
+        user_geo_data.save()
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching geo data: {e}")
+
+
+
+@shared_task
+def fetch_and_save_holiday_data(id):
+    try:
+        user_geo_data = UserGeoData.objects.get(id=id)
+        country_code = user_geo_data.country_code
+
+        current_date = datetime.date.today()
+        year = current_date.year
+        month = current_date.month
+        day = current_date.day
+
+        holiday_api_url = f'{HOLIDAY_API_BASE_URL}&country={country_code}&year={year}&month={month}&day={day}'
+
+        response = requests.get(holiday_api_url)
+        response.raise_for_status()
+        data = response.json()
+        holiday_name = data['name']
+        holiday_type = data['type']
+        holiday, created = Holiday.objects.update_or_create(
+            user=user_geo_data.user,
+            defaults={
+                'holiday_name': holiday_name,
+                'holiday_type': holiday_type,
+            }
+        )
+    except (UserGeoData.DoesNotExist, requests.exceptions.RequestException) as e:
+        print(f"Error fetching holiday data: {e}")
