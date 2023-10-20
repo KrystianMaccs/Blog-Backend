9288bd71b02ab52fbdda190126bf293752befa0f
Krystian Maccs <krystianmaccs@gmail.com>
2023-10-20 19:30:29 +0100

    feat: fix celery tasks

--- accounts/signals.py
+++ accounts/signals.py
@@ -3,7 +3,7 @@ import logging
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from blog.settings.base import AUTH_USER_MODEL
-from .tasks import fetch_and_save_geodata, fetch_and_save_holiday_data
+from .tasks import fetch_and_save_geodata#, fetch_and_save_holiday_data
 
 logger = logging.getLogger(__name__)
 
@@ -11,6 +11,6 @@ logger = logging.getLogger(__name__)
 @receiver(post_save, sender=AUTH_USER_MODEL)
 def trigger_celery_tasks(sender, instance, created, **kwargs):
     if created:
-        fetch_and_save_geodata.delay()
-        fetch_and_save_holiday_data.delay(instance.id)
+        fetch_and_save_geodata.delay(instance.id)
+        #fetch_and_save_holiday_data.delay(instance.id)
         logger.info(f"{instance}'s profile created")
--- accounts/tasks.py
+++ accounts/tasks.py
@@ -1,68 +1,113 @@
 import requests
 from celery import shared_task
-from .models import UserGeoData, Holiday
+from .models import Holiday, UserGeoData, User
 import datetime
+import json
 
 
 
 GEO_API_URL = 'https://ipgeolocation.abstractapi.com/v1/?api_key=d77f75a5593e4073a6eb1bef9c35e929'
 HOLIDAY_API_BASE_URL = 'https://holidays.abstractapi.com/v1/?api_key=9cb9967be3054f95a7f4c80fae708370'
 
-@shared_task
-def fetch_and_save_geodata():
-    try:
-        response = requests.get(GEO_API_URL)
-        response.raise_for_status()
 
+@shared_task
+def fetch_and_save_geodata(user_id):
+    user = User.objects.get(id=user_id)
+    
+    response = requests.get(GEO_API_URL)
+    if response.status_code == 200:
         data = response.json()
         
-        ip_address = data['ip_address']
-        city = data['city']
-        region_iso_code = data['region_iso_code']
-        country_code = data['country_code']
-        longitude = data['longitude']
-        latitude = data['latitude']
-
-        user_geo_data, created = UserGeoData.objects.update_or_create(
-            ip_address=ip_address,
-            defaults={
-                'city': city,
-                'region_iso_code': region_iso_code,
-                'country_code': country_code,
-                'longitude': longitude,
-                'latitude': latitude,
-            }
+        user_geo_data = UserGeoData(
+            user=user,
+            ip_address=data["ip_address"],
+            city=data["city"],
+            region_iso_code=data["region_iso_code"],
+            country_code=data["country_code"],
+            longitude=data["longitude"],
+            latitude=data["latitude"],
         )
         user_geo_data.save()
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching geo data: {e}")
+    # try:
+    #     response = requests.get(GEO_API_URL)
+    #     data = response.json()
+        
+    #     ip_address = data['ip_address']
+    #     city = data['city']
+    #     region_iso_code = data['region_iso_code']
+    #     country_code = data['country_code']
+    #     longitude = data['longitude']
+    #     latitude = data['latitude']
+
+    #     user_geo_data, created = UserGeoData.objects.update_or_create(
+    #         ip_address=ip_address,
+    #         defaults={
+    #             'city': city,
+    #             'region_iso_code': region_iso_code,
+    #             'country_code': country_code,
+    #             'longitude': longitude,
+    #             'latitude': latitude,
+    #         }
+    #     )
+    #     user_geo_data.save()
+    # except requests.exceptions.RequestException as e:
+    #     print(f"Error fetching geo data: {e}")
 
 
 
-@shared_task
-def fetch_and_save_holiday_data(id):
-    try:
-        user_geo_data = UserGeoData.objects.get(id=id)
-        country_code = user_geo_data.country_code
+# @shared_task
+# def fetch_and_save_holiday_data(id):
+#     try:
+#         user_geo_data = UserGeoData.objects.get(id=id)
+#         country_code = user_geo_data.country_code
 
-        current_date = datetime.date.today()
-        year = current_date.year
-        month = current_date.month
-        day = current_date.day
+#         current_date = datetime.date.today()
+#         year = current_date.year
+#         month = current_date.month
+#         day = current_date.day
 
-        holiday_api_url = f'{HOLIDAY_API_BASE_URL}&country={country_code}&year={year}&month={month}&day={day}'
+#         holiday_api_url = f'{HOLIDAY_API_BASE_URL}&country={country_code}&year={year}&month={month}&day={day}'
 
-        response = requests.get(holiday_api_url)
-        response.raise_for_status()
-        data = response.json()
-        holiday_name = data['name']
-        holiday_type = data['type']
-        holiday, created = Holiday.objects.update_or_create(
-            user=user_geo_data.user,
-            defaults={
-                'holiday_name': holiday_name,
-                'holiday_type': holiday_type,
-            }
-        )
-    except (UserGeoData.DoesNotExist, requests.exceptions.RequestException) as e:
-        print(f"Error fetching holiday data: {e}")
+#         response = requests.get(holiday_api_url)
+#         response.raise_for_status()
+#         data = response.json()
+#         holiday_name = data['name']
+#         holiday_type = data['type']
+#         holiday, created = Holiday.objects.update_or_create(
+#             user=user_geo_data.user,
+#             defaults={
+#                 'holiday_name': holiday_name,
+#                 'holiday_type': holiday_type,
+#             }
+#         )
+#     except (UserGeoData.DoesNotExist, requests.exceptions.RequestException) as e:
+#         print(f"Error fetching holiday data: {e}")
+
+# @shared_task
+# def fetch_and_save_holiday_data(id):
+#     try:
+#         user_geo_data = UserGeoData.objects.get(id=id)
+#     except UserGeoData.DoesNotExist:
+#         print(f"UserGeoData with id {id} does not exist.")
+#         return
+#     country_code = user_geo_data.country_code
+#     current_date = datetime.date.today()
+#     year = current_date.year
+#     month = current_date.month
+#     day = current_date.day
+#     holiday_api_url = f'{HOLIDAY_API_BASE_URL}&country={country_code}&year={year}&month={month}&day={day}'
+#     try:
+#         response = requests.get(holiday_api_url)
+#         response.raise_for_status()
+#         data = response.json()
+#         holiday_name = data['name']
+#         holiday_type = data['type']
+#         holiday, created = Holiday.objects.update_or_create(
+#             user=user_geo_data.user,
+#             defaults={
+#                 'holiday_name': holiday_name,
+#                 'holiday_type': holiday_type,
+#             }
+#         )
+#     except requests.exceptions.RequestException as e:
+#         print(f"Error fetching holiday data: {e}")
